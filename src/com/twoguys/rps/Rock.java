e5196846d278162b3ee0c6d50ad85de2fcdfcf0a
Nate Young <youngnh@gmail.com>
2009-12-11 11:32:10 -0600

    second test rock does not beat paper

--- src/com/twoguys/rps/Rock.java
+++ src/com/twoguys/rps/Rock.java
@@ -2,4 +2,12 @@ package com.twoguys.rps;
 
 public class Rock extends Throw {
 
+    public boolean beats(Throw other) {
+	if(other.getClass() == Scissors.class) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
 }
