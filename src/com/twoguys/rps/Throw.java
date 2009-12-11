c932f635ec8816dcc5efbc101869923953bfc2a5
Nate Young <youngnh@gmail.com>
2009-12-11 11:43:09 -0600

    final tests for core logic pulled the individual case out into an abstract method and each class now simply declares which other class it pwns

--- src/com/twoguys/rps/Throw.java
+++ src/com/twoguys/rps/Throw.java
@@ -3,7 +3,12 @@ package com.twoguys.rps;
 public abstract class Throw {
 
     public boolean beats(Throw other) {
-	return true;
+	if(other.getClass() == getPwns()) {
+	    return true;
+	} else {
+	    return false;
+	}
     }
 
+    public abstract Class<? extends Throw> getPwns();
 }
