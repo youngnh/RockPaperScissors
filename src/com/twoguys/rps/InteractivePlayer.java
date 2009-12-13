85d599e171883c2b9e56296e3262b3a1f0048edf
Nate Young <youngnh@gmail.com>
2009-12-13 14:39:19 -0600

    InteractivePlayer prompts the user for input

--- src/com/twoguys/rps/InteractivePlayer.java
+++ src/com/twoguys/rps/InteractivePlayer.java
@@ -4,10 +4,11 @@ import java.io.*;
 
 public class InteractivePlayer implements Player {
 
-    public InteractivePlayer(InputStream fromPlayer, OutputStream toPlayer) {
-	PrintWriter out = new PrintWriter(toPlayer);
-	out.write("Player 1 Name: ");
-	out.flush();
+    public InteractivePlayer(Reader fromPlayer, Writer toPlayer) throws IOException {
+	toPlayer.write("Player 1 Name: ");
+	toPlayer.flush();
+	LineNumberReader in = new LineNumberReader(fromPlayer);
+	in.readLine();
     }
 
     public String getName() {
--- test/com/twoguys/rps/InteractivePlayerTest.java
+++ test/com/twoguys/rps/InteractivePlayerTest.java
@@ -9,15 +9,29 @@ import org.junit.*;
 public class InteractivePlayerTest {
 
     public final static String P1_PROMPT = "Player 1 Name: ";
+    private String nate = "Nate";
 
     @Test
-    public void testPromptForName() {
-	InputStream in = mock(InputStream.class);
-	OutputStream out = new ByteArrayOutputStream();
+    public void testPromptsForName() throws Exception {
+	Reader in = new StringReader(nate + "\n");
+	Writer out = new CharArrayWriter();
 
 	Player player = new InteractivePlayer(in, out);
 
 	String written = out.toString();
 	assertEquals(P1_PROMPT, written);
+    }
+
+    @Test
+    public void testReadsNameFromInput() throws Exception {
+	Reader in = new StringReader(nate + "\n");
+	Writer out = new CharArrayWriter();
+
+	Player player = new InteractivePlayer(in, out);
+
+	String written = out.toString();
+	assertEquals(P1_PROMPT, written);
+
+	assertEquals(-1, in.read());
     }
 }
