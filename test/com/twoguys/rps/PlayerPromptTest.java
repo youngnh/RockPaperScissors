5b1887d907c68fa457c5710d56a73c8d7cd14f7a
Nate Young <youngnh@gmail.com>
2010-01-28 19:40:32 -0600

    Implemented the PlayerPrompt

---
+++ test/com/twoguys/rps/PlayerPromptTest.java
@@ -0,0 +1,36 @@
+package com.twoguys.rps;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.*;
+import org.junit.*;
+
+public class PlayerPromptTest {
+
+    @Test
+    public void testPlayerPromptPrintsMessage() throws Exception {
+	String expected = "Player 1 Name: ";
+	StringReader in = new StringReader("jabberwocky");
+	StringWriter out = new StringWriter();
+
+	PlayerPrompt prompt = new PlayerPrompt(in, out, 1);
+	prompt.prompt();
+
+	assertEquals(expected, out.toString());
+    }
+
+     @Test
+    public void testPlayerHasNameGivenInInput() throws Exception {
+	String expected = "Player 1 Name: ";
+	String input = "jabberwocky";
+	StringReader in = new StringReader(input);
+	StringWriter out = new StringWriter();
+
+	PlayerPrompt prompt = new PlayerPrompt(in, out, 1);
+	Player player = prompt.prompt();
+
+	assertEquals(input, player.getName());
+    }   
+
+}
