52a1a66aa9d0af1c1b6b4a604c05c16e367ae9e3
Nate Young <nyoung@revelytix.com>
2010-09-25 13:55:38 -0500

    play a round

--- rps/src/rps/core.clj
+++ rps/src/rps/core.clj
@@ -8,4 +8,21 @@
   (print prompt "Name: "))
 
 (defn prompt-for-throw []
-  (print "[R]ock, [P]aper, or [S]cissors? "))
+  (print "[R]ock, [P]aper, or [S]cissors? ")
+  (let [response (read-line)]
+    (condp = response
+       "R" :rock
+       "P" :paper
+       "S" :scissors)))
+
+(defn beats [throw other]
+  (= other (get {:rock :scissors
+		 :scissors :paper
+		 :paper :rock} throw)))
+
+(defn play-round []
+  (let [throw (prompt-for-throw)
+	other (prompt-for-throw)]
+    (cond (beats throw other) [1 0]
+	  (beats other throw) [0 1]
+	  :otherwise [0 0])))
--- rps/test/rps/test/core.clj
+++ rps/test/rps/test/core.clj
@@ -1,8 +1,9 @@
 (ns rps.test.core
   (:use [rps.core] :reload)
   (:use [clojure.test]
+	[clojure.string :only (join)]
 	[clojure.contrib.io :only (reader)])
-  (:import [java.io StringWriter]))
+  (:import [java.io StringReader StringWriter]))
 
 (deftest test-scripted-games
   (let [string-writer (StringWriter.)
@@ -20,7 +21,59 @@
       (is (= "Player 1 Name: " (.toString string-writer))))))
 
 (deftest test-prompt-for-throw
-  (let [string-writer (StringWriter.)]
-    (binding [*out* string-writer]
-      (prompt-for-throw)
-      (is (= "[R]ock, [P]aper, or [S]cissors? " (.toString string-writer))))))
+  (testing "prints prompt"
+    (let [string-writer (StringWriter.)]
+      (binding [*in* (reader (StringReader. "R\n"))
+		*out* string-writer]
+       (prompt-for-throw)
+       (is (= "[R]ock, [P]aper, or [S]cissors? " (.toString string-writer))))))
+
+  (testing "reads response, returns throw"
+    (binding [*in* (reader (StringReader. "R\nP\nS\n"))
+	      *out* (StringWriter.)]
+      (is (= :rock (prompt-for-throw)))
+      (is (= :paper (prompt-for-throw)))
+      (is (= :scissors (prompt-for-throw))))))
+
+(def starts-with? (memfn startsWith prefix))
+
+(deftest test-beats
+  (testing "R > S > P > R"
+    (are [throw other] (beats throw other)
+	 [:rock :scissors]
+	 [:scissors :paper]
+	 [:paper :rock]))
+
+  (testing "R < P < S < R"
+    (are [throw other] (not (beats throw other))
+	 [:rock :paper]
+	 [:paper :scissors]
+	 [:scissors :rock])))
+
+(deftest test-play-round
+  (testing "prompts both players for a throw"
+    (let [string-writer (StringWriter.)]
+      (binding [*in* (reader (StringReader. "R\nS\n"))
+		*out* string-writer]
+	(play-round)
+	(is (starts-with? (.toString string-writer)
+			  (join (take 2 (repeat "[R]ock, [P]aper, or [S]cissors? "))))))))
+
+  (testing "returns score based on who had wining throw"
+    (let [rock-v-scissors (reader (StringReader. "R\nS\n"))
+	  paper-v-scissors (reader (StringReader. "P\nS\n"))
+	  rock-v-rock (reader (StringReader. "R\nR\n"))]
+      (testing "Rock v. Scissors: rock wins"
+	(binding [*in* rock-v-scissors
+		 *out* (StringWriter.)]
+	 (is (= [1 0] (play-round)))))
+
+      (testing "Paper v. Scissors: paper loses"
+	(binding [*in* paper-v-scissors
+		  *out* (StringWriter.)]
+	  (is (= [0 1] (play-round)))))
+
+      (testing "Rock v. Rock: tie"
+	(binding [*in* rock-v-rock
+		  *out* (StringWriter.)]
+	  (is (= [0 0] (play-round))))))))
